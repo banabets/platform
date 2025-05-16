37d851cb6d80434cb83b51e968b09ec7a4e94c89
Banabets <gummy-penal-0o@icloud.com>
2025-05-16 04:51:03 -0400

    Update useRecentPlays.ts

--- src/sections/RecentPlays/useRecentPlays.ts
+++ src/sections/RecentPlays/useRecentPlays.ts
@@ -1,10 +1,8 @@
-
 import { GambaTransaction } from 'gamba-core-v2'
 import { useGambaEventListener, useGambaEvents, useWalletAddress } from 'gamba-react-v2'
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { PLATFORM_CREATOR_ADDRESS } from '../../constants'
-import { Connection } from '@solana/web3.js'
 
 interface Params {
   showAllPlatforms?: boolean
@@ -15,43 +13,37 @@ export function useRecentPlays(params: Params = {}) {
   const location = useLocation()
   const userAddress = useWalletAddress()
 
+  // Fetch previous events
   const previousEvents = useGambaEvents(
     'GameSettled',
     { address: !showAllPlatforms ? PLATFORM_CREATOR_ADDRESS : undefined },
   )
 
   const [newEvents, setEvents] = React.useState<GambaTransaction<'GameSettled'>[]>([])
-  const connection = new Connection("https://api.mainnet-beta.solana.com")
 
+  // Listen for new events
   useGambaEventListener(
     'GameSettled',
-    async (event) => {
+    (event) => {
+      // Ignore events that occured on another platform
       if (!showAllPlatforms && !event.data.creator.equals(PLATFORM_CREATOR_ADDRESS)) return
-
-      const ignoredWallet = "2fop1Dg4SqeKSt9oZEF2caCfVurxzzwmMuTsVtACv4fX"
-      const isFromIgnored = event.data.user.toBase58() === ignoredWallet
-
+      // Set a delay on games with suspenseful reveal
       const delay = event.data.user.equals(userAddress) && ['plinko', 'slots'].some((x) => location.pathname.includes(x)) ? 3000 : 1
-
-      setTimeout(async () => {
-        const alreadyExists = newEvents.some(e => e.signature === event.signature)
-        if (alreadyExists || isFromIgnored) return
-
-        try {
-          const statusResp = await connection.getSignatureStatus(event.signature)
-          const confirmed = statusResp?.value?.confirmationStatus === "finalized" && !statusResp?.value?.err
-
-          if (!confirmed) return
+      setTimeout(
+        () => {
           setEvents((events) => [event, ...events])
-        } catch (error) {
-          console.error("Error verificando transacción:", error)
-        }
-      }, delay)
+        },
+        delay,
+      )
     },
     [location.pathname, userAddress, showAllPlatforms],
   )
 
-  return React.useMemo(() => {
-    return [...newEvents, ...previousEvents]
-  }, [newEvents, previousEvents])
+  // Merge previous & new events
+  return React.useMemo(
+    () => {
+      return [...newEvents, ...previousEvents]
+    },
+    [newEvents, previousEvents],
+  )
 }
