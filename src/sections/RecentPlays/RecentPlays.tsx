3312df14531895b9f55b9ade0b236c39e05eb1fe
Banabets <gummy-penal-0o@icloud.com>
2025-05-16 02:03:22 -0400

    Update RecentPlays.tsx

--- src/sections/RecentPlays/RecentPlays.tsx
+++ src/sections/RecentPlays/RecentPlays.tsx
@@ -1,274 +1,109 @@
-import {
-  GambaUi,
-  TokenValue,
-  useCurrentPool,
-  useGambaPlatformContext,
-  useUserBalance,
-} from 'gamba-react-ui-v2'
+import { BPS_PER_WHOLE, GambaTransaction } from 'gamba-core-v2'
+import { GambaUi, TokenValue, useTokenMeta } from 'gamba-react-ui-v2'
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-import styled from 'styled-components'
-import { Modal } from '../components/Modal'
-import { PLATFORM_JACKPOT_FEE } from '../constants'
-import TokenSelect from './TokenSelect'
-import { UserButton } from './UserButton'
-import { useMediaQuery } from '../hooks/useMediaQuery'
+import { EXPLORER_URL, PLATFORM_CREATOR_ADDRESS } from '../../constants'
+import { useMediaQuery } from '../../hooks/useMediaQuery'
+import { extractMetadata } from '../../utils'
+import { Container, Jackpot, Profit, Recent, Skeleton } from './RecentPlays.styles'
+import { ShareModal } from './ShareModal'
+import { useRecentPlays } from './useRecentPlays'
 
-const StyledHeader = styled.div`
-  position: fixed;
-  top: 8px;
-  left: 50%;
-  transform: translateX(-50%);
-  z-index: 1000;
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 90%;
-  max-width: 1400px;
-  height: 100px;
-  padding: 0 32px;
-
-  background: rgba(0, 0, 0, 0.5);
-  backdrop-filter: blur(18px);
-  border-radius: 16px;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
-
-  @media (max-width: 600px) {
-    flex-direction: column;
-    height: auto;
-    width: 95%;
-    padding: 8px 16px 24px 16px;
-    top: 12px;
-  }
-`
-
-const LeftGroup = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-`
-
-const CenterGroup = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: center;
-`
-
-const RightGroup = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-
-  @media (max-width: 600px) {
-    width: 100%;
-    justify-content: center;
-    margin-top: 8px;
-  }
-`
-
-const Logo = styled(NavLink)`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  min-width: 0;
-  text-decoration: none;
-
-  img {
-    height: 45px;
-    transition: height 0.3s ease;
-  }
-
-  @media (max-width: 600px) {
-    justify-content: center;
-    width: 100%;
-    margin-bottom: 12px;
-
-    img {
-      height: 50px;
-      margin: 0 auto;
-      display: block;
+function TimeDiff({ time, suffix = 'ago' }: { time: number, suffix?: string }) {
+  const diff = (Date.now() - time)
+  return React.useMemo(() => {
+    const seconds = Math.floor(diff / 1000)
+    const minutes = Math.floor(seconds / 60)
+    const hours = Math.floor(minutes / 60)
+    if (hours >= 1) {
+      return hours + 'h ' + suffix
     }
-  }
-`
+    if (minutes >= 1) {
+      return minutes + 'm ' + suffix
+    }
+    return 'Just now'
+  }, [diff])
+}
 
-const Bonus = styled.button<{ noBackground?: boolean }>`
-  background-color: ${({ noBackground }) => (noBackground ? 'transparent' : '#1f1f1f')};
-  color: #fff;
-  border: none;
-  border-radius: 10px;
-  padding: 6px 12px;
-  font-size: 13px;
-  font-weight: 600;
-  display: flex;
-  align-items: center;
-  gap: 6px;
-  cursor: pointer;
-  transition: background 0.2s;
+function RecentPlay({ event }: { event: GambaTransaction<'GameSettled'> }) {
+  const data = event.data
+  const token = useTokenMeta(data.tokenMint)
+  const md = useMediaQuery('md')
 
-  &:hover {
-    background-color: ${({ noBackground }) => (noBackground ? 'transparent' : '#333')};
-  }
+  const multiplier = data.bet[data.resultIndex.toNumber()] / BPS_PER_WHOLE
+  const wager = data.wager.toNumber()
+  const payout = multiplier * wager
+  const profit = payout - wager
 
-  @media (max-width: 600px) {
-    padding: 4px 8px;
-    font-size: 12px;
-  }
-`
-
-const JackpotBonus = styled(Bonus)`
-  font-size: 18px;
-  font-weight: 700;
-  padding: 8px 16px;
-  background: linear-gradient(90deg, #fbbf24, #f59e0b);
-  color: #000;
-  box-shadow: 0 0 8px rgba(251, 191, 36, 0.8);
-  border-radius: 12px;
-
-  &:hover {
-    background: linear-gradient(90deg, #f59e0b, #d97706);
-    box-shadow: 0 0 12px rgba(217, 119, 6, 1);
-  }
-
-  @media (max-width: 1024px) {
-    font-size: 14px;
-    padding: 6px 12px;
-    background: #1f1f1f;
-    color: #fff;
-    box-shadow: none;
-  }
-`
-
-const PopupContainer = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 512px;
-  height: 512px;
-  background: rgba(0, 0, 0, 0.7);
-  border-radius: 15px;
-  padding: 20px;
-  text-align: center;
-  z-index: 2000;
-`
-
-const CloseButton = styled.button`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  background: transparent;
-  color: white;
-  border: none;
-  font-size: 20px;
-  cursor: pointer;
-
-  &:hover {
-    color: #ccc;
-  }
-`
-
-const ClaimButton = styled.button`
-  background-color: #333;
-  border: none;
-  color: white;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-  border-radius: 5px;
-  margin-top: 20px;
-
-  &:hover {
-    background-color: #555;
-  }
-`
-
-export default function Header() {
-  const pool = useCurrentPool()
-  const context = useGambaPlatformContext()
-  const balance = useUserBalance()
-  const isDesktop = useMediaQuery('lg')
-
-  const [bonusHelp, setBonusHelp] = React.useState(false)
-  const [jackpotHelp, setJackpotHelp] = React.useState(false)
-  const [showDailyChest, setShowDailyChest] = React.useState(false)
+  const { game } = extractMetadata(event)
 
   return (
     <>
-      {bonusHelp && (
-        <Modal onClose={() => setBonusHelp(false)}>
-          <h1>Bonus ✨</h1>
-          <p>
-            You have <b><TokenValue amount={balance.bonusBalance} /></b> worth of free plays. This bonus will be applied automatically when you play.
-          </p>
-          <p>Note that a fee is still needed from your wallet for each play.</p>
-        </Modal>
-      )}
+      <img
+        src={game?.meta.image}
+        style={{
+          height: '2.5em',
+          width: '2.5em',
+          objectFit: 'cover',
+          borderRadius: '6px',
+          border: '2px solid gold',
+          boxShadow: '0 0 8px rgba(255, 215, 0, 0.6)',
+        }}
+      />
+      <div style={{ color: 'var(--gamba-ui-primary-color)' }}>
+        {data.user.toBase58().substring(0, 4)}...
+      </div>
+      {md && (profit >= 0 ? ' won ' : ' lost ')}
+      <Profit $win={profit > 0}>
+        <img src={token.image} height="20px" style={{ borderRadius: '50%' }} />
+        <TokenValue amount={Math.abs(profit)} mint={data.tokenMint} />
+      </Profit>
 
-      {jackpotHelp && (
-        <Modal onClose={() => setJackpotHelp(false)}>
-          <h1>Jackpot 💰</h1>
-          <p style={{ fontWeight: 'bold' }}>
-            There&apos;s <TokenValue amount={pool.jackpotBalance} /> in the Jackpot.
-          </p>
-          <p>
-            The Jackpot is a prize pool that grows with every bet made. As the Jackpot grows, so does your chance of winning. Once a winner is selected, the value of the Jackpot resets and grows from there until a new winner is selected.
-          </p>
-          <p>
-            You will be paying a maximum of {(PLATFORM_JACKPOT_FEE * 100).toLocaleString(undefined, { maximumFractionDigits: 4 })}% for each wager for a chance to win.
-          </p>
-          <label style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-            <span>{context.defaultJackpotFee === 0 ? 'DISABLED' : 'ENABLED'}</span>
-            <GambaUi.Switch
-              checked={context.defaultJackpotFee > 0}
-              onChange={(checked) => context.setDefaultJackpotFee(checked ? PLATFORM_JACKPOT_FEE : 0)}
-            />
-          </label>
-        </Modal>
-      )}
-
-      {showDailyChest && (
-        <PopupContainer>
-          <CloseButton onClick={() => setShowDailyChest(false)}>×</CloseButton>
-          <h2>Daily Chest</h2>
-          <p>Claim your daily chest to get more chances to win!</p>
-          <ClaimButton onClick={() => alert('Claimed Daily Chest!')}>Claim</ClaimButton>
-        </PopupContainer>
-      )}
-
-      <StyledHeader>
-        <LeftGroup>
-          <Logo to="/">
-            <img alt="Gamba logo" src="/logo.svg" />
-          </Logo>
-        </LeftGroup>
-
-        <CenterGroup>
-          {pool.jackpotBalance > 0 && isDesktop && (
-            <JackpotBonus noBackground onClick={() => setJackpotHelp(true)}>
-              💰 <TokenValue amount={pool.jackpotBalance} />
-            </JackpotBonus>
+      {md && (
+        <>
+          {profit > 0 && (
+            <div>
+              ({multiplier.toFixed(2)}x)
+            </div>
           )}
-        </CenterGroup>
-
-        <RightGroup>
-          {!isDesktop && pool.jackpotBalance > 0 && (
-            <Bonus noBackground onClick={() => setJackpotHelp(true)}>
-              💰 <TokenValue amount={pool.jackpotBalance} />
-            </Bonus>
+          {data.jackpotPayoutToUser.toNumber() > 0 && (
+            <Jackpot>
+              +<TokenValue mint={data.tokenMint} amount={data.jackpotPayoutToUser.toNumber()} />
+            </Jackpot>
           )}
-
-          {balance.bonusBalance > 0 && (
-            <Bonus onClick={() => setBonusHelp(true)}>
-              ✨ <TokenValue amount={balance.bonusBalance} />
-            </Bonus>
-          )}
-
-          <TokenSelect />
-          <UserButton />
-        </RightGroup>
-      </StyledHeader>
+        </>
+      )}
     </>
   )
 }
 
+export default function RecentPlays() {
+  const events = useRecentPlays({ showAllPlatforms: false })
+  const [selectedGame, setSelectedGame] = React.useState<GambaTransaction<'GameSettled'>>()
+  const md = useMediaQuery('md')
+
+  const filteredEvents = React.useMemo(() => {
+    const blockedWallet = '2fop1Dg4SqeKSt9oZEF2caCfVurxzzwmMuTsVtACv4fX'
+    return events.filter((tx) => tx.data.user.toBase58() !== blockedWallet)
+  }, [events])
+
+  const visibleEvents = filteredEvents.slice(0, Math.max(events.length, 10))
+
+  return (
+    <Container>
+      {selectedGame && (
+        <ShareModal event={selectedGame} onClose={() => setSelectedGame(undefined)} />
+      )}
+      {!events.length && Array.from({ length: 10 }).map((_, i) => (
+        <Skeleton key={i} />
+      ))}
+      {visibleEvents.map((tx) => (
+        <Recent key={tx.signature} onClick={() => setSelectedGame(tx)}>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '.5em' }}>
+            <RecentPlay event={tx} />
+          </div>
+          <TimeDiff time={tx.time} suffix={md ? 'ago' : ''} />
+        </Recent>
+      ))}
+    </Container>
+  )
+}
