14c02793dcba48e2acd949cf89acea18eaf19325
Banabets <gummy-penal-0o@icloud.com>
2025-05-16 04:42:38 -0400

    Update useRecentPlays.ts

--- src/sections/RecentPlays/useRecentPlays.ts
+++ src/sections/RecentPlays/useRecentPlays.ts
@@ -4,6 +4,7 @@ import { useGambaEventListener, useGambaEvents, useWalletAddress } from 'gamba-r
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { PLATFORM_CREATOR_ADDRESS } from '../../constants'
+import { Connection } from '@solana/web3.js'
 
 interface Params {
   showAllPlatforms?: boolean
@@ -14,39 +15,42 @@ export function useRecentPlays(params: Params = {}) {
   const location = useLocation()
   const userAddress = useWalletAddress()
 
-  // Fetch previous events
   const previousEvents = useGambaEvents(
     'GameSettled',
     { address: !showAllPlatforms ? PLATFORM_CREATOR_ADDRESS : undefined },
   )
 
   const [newEvents, setEvents] = React.useState<GambaTransaction<'GameSettled'>[]>([])
+  const connection = new Connection("https://api.mainnet-beta.solana.com")
 
-  // Listen for new events
   useGambaEventListener(
     'GameSettled',
-    (event) => {
-      // Ignore events that occured on another platform
+    async (event) => {
       if (!showAllPlatforms && !event.data.creator.equals(PLATFORM_CREATOR_ADDRESS)) return
 
-      // Opcional: ignorar eventos de una wallet problemática
       const ignoredWallet = "2fop1Dg4SqeKSt9oZEF2caCfVurxzzwmMuTsVtACv4fX"
       const isFromIgnored = event.data.user.toBase58() === ignoredWallet
 
       const delay = event.data.user.equals(userAddress) && ['plinko', 'slots'].some((x) => location.pathname.includes(x)) ? 3000 : 1
 
-      setTimeout(() => {
-        setEvents((events) => {
-          const alreadyExists = events.some(e => e.signature === event.signature)
-          if (alreadyExists || isFromIgnored) return events
-          return [event, ...events]
-        })
+      setTimeout(async () => {
+        const alreadyExists = newEvents.some(e => e.signature === event.signature)
+        if (alreadyExists || isFromIgnored) return
+
+        try {
+          const statusResp = await connection.getSignatureStatus(event.signature)
+          const confirmed = statusResp?.value?.confirmationStatus === "finalized" && !statusResp?.value?.err
+
+          if (!confirmed) return
+          setEvents((events) => [event, ...events])
+        } catch (error) {
+          console.error("Error verificando transacción:", error)
+        }
       }, delay)
     },
     [location.pathname, userAddress, showAllPlatforms],
   )
 
-  // Merge previous & new events
   return React.useMemo(() => {
     return [...newEvents, ...previousEvents]
   }, [newEvents, previousEvents])
