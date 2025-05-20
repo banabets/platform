0b681f42305fd21edfd793f80c6704bae7f9d8a3
doctorsolana <hashinja@protonmail.com>
2025-05-20 18:25:57 +0200

    added back trollbox

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Header from './sections/Header'
 import RecentPlays from './sections/RecentPlays/RecentPlays'
 import Toasts from './sections/Toasts'
 import { MainWrapper, TosInner, TosWrapper } from './styles'
+import TrollBox from './components/TrollBox'
 
 function ScrollToTop() {
   const { pathname } = useLocation()
@@ -80,6 +81,7 @@ export default function App() {
         <h2 style={{ textAlign: 'center' }}>Recent Plays</h2>
         <RecentPlays />
       </MainWrapper>
+      <TrollBox />
     </>
   )
 }
