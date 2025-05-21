37d1aae8b9649ca4d30fd13935dcecfecf724d8c
Banabets <gummy-penal-0o@icloud.com>
2025-05-21 04:37:31 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 import react from '@vitejs/plugin-react'
+
+import { VitePWA } from 'vite-plugin-pwa'
+
 import { defineConfig } from 'vite'
 
 const ENV_PREFIX = ['VITE_']
@@ -16,6 +19,30 @@ export default defineConfig(() => ({
     },
   },
   plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt'],
+      manifest: {
+        name: 'Banabets',
+        short_name: 'Banabets',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#512da8',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
     react({ jsxRuntime: 'classic' }),
   ],
 }))
