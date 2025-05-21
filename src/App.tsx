e78ef9ca1d4a89cb386a8655cbf31f27e7a21890
Banabets <gummy-penal-0o@icloud.com>
2025-05-21 05:20:21 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { useWalletModal } from '@solana/wallet-adapter-react-ui'
 import { GambaUi } from 'gamba-react-ui-v2'
 import { useTransactionError } from 'gamba-react-v2'
@@ -15,7 +14,6 @@ import RecentPlays from './sections/RecentPlays/RecentPlays'
 import Toasts from './sections/Toasts'
 import { MainWrapper, TosInner, TosWrapper } from './styles'
 import TrollBox from './components/TrollBox'
-import { WalletConnectionProvider } from './wallet' // NUEVA LÍNEA
 
 function ScrollToTop() {
   const { pathname } = useLocation()
@@ -55,7 +53,7 @@ export default function App() {
   const set = useUserStore((state) => state.set)
 
   return (
-    <WalletConnectionProvider>
+    <>
       {newcomer && (
         <Modal>
           <h1>Welcome</h1>
@@ -73,7 +71,7 @@ export default function App() {
       <ScrollToTop />
       <ErrorHandler />
       <Header />
-      <div style={{ height: '50px', visibility: 'hidden', pointerEvents: 'none' }} />
+<div style={{ height: '50px', visibility: 'hidden', pointerEvents: 'none' }} />
       <Toasts />
       <MainWrapper>
         <Routes>
@@ -84,6 +82,6 @@ export default function App() {
         <RecentPlays />
       </MainWrapper>
       <TrollBox />
-    </WalletConnectionProvider>
+    </>
   )
 }
