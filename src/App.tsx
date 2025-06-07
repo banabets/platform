90de02d1101b0f0db2e8a85c87df1c22ca07473e
Banabets <gummy-penal-0o@icloud.com>
2025-06-07 19:16:01 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { useWalletModal } from '@solana/wallet-adapter-react-ui'
 import { GambaUi } from 'gamba-react-ui-v2'
 import { useTransactionError } from 'gamba-react-v2'
@@ -17,6 +16,16 @@ import { MainWrapper, TosInner, TosWrapper } from './styles'
 import TrollBox from './components/TrollBox'
 import LeaderboardsModal from './components/LeaderboardsModal'
 import Sidebar from './components/Sidebar'
+import styled from 'styled-components'
+
+const MainContentWrapper = styled.div`
+  flex: 1;
+  margin-left: 260px;
+
+  @media (max-width: 768px) {
+    margin-left: 0;
+  }
+`
 
 function ScrollToTop() {
   const { pathname } = useLocation()
@@ -43,7 +52,7 @@ function ErrorHandler() {
     <>
       {error && (
         <Modal onClose={() => setError(undefined)}>
-          <h1>Error occured</h1>
+          <h1>Error occurred</h1>
           <p>{error.message}</p>
         </Modal>
       )}
@@ -58,7 +67,7 @@ export default function App() {
   return (
     <div style={{ display: 'flex' }}>
       <Sidebar />
-      <div style={{ flex: 1 }}>
+      <MainContentWrapper>
         {newcomer && (
           <Modal>
             <h1>Welcome</h1>
@@ -87,7 +96,7 @@ export default function App() {
           <RecentPlays />
         </MainWrapper>
         <TrollBox />
-      </div>
+      </MainContentWrapper>
     </div>
   )
 }
