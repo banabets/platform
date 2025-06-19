f59b1b54f57d87d4562f95fcadb89d5f47fab779
Bananas <gummy-penal-0o@icloud.com>
2025-06-19 18:20:13 -0400

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -125,7 +125,7 @@ const ClaimButton = styled.button`
 
 const Sidebar = () => {
   const [solPrice, setSolPrice] = useState(null)
-  const airdropAmount = 5.13
+  const airdropAmount = 5.20
 
   useEffect(() => {
     const fetchPrice = async () => {
