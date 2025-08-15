28954647f3c8958a0e388168180cfa05193b2991
Bananas <gummy-penal-0o@icloud.com>
2025-08-15 06:21:03 -0400

    Update config.ts

--- src/games/Jackpot/config.ts
+++ src/games/Jackpot/config.ts
@@ -2,7 +2,7 @@ import { PublicKey } from '@solana/web3.js';
 import { WRAPPED_SOL_MINT } from '@gamba-labs/multiplayer-sdk';
 
 //gamba creator bot adddress
-export const DESIRED_CREATOR = new PublicKey('GamermKQpHDVzw8BwuQnj6XSYXqvwCFu64k9cn88umqn');
+export const DESIRED_CREATOR = new PublicKey('Etnd3K8ZkMoUivezmxxaRkZBvVFTAvuQftpfvLyjhjBp');
 
 export const DESIRED_MAX_PLAYERS = 999;
 export const DESIRED_WINNERS_TARGET = 1;
